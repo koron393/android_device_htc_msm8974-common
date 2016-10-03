a2968eaa0c16b9f78520bbe8fc2548ef080a760e
Kousuke Nagatsuka <koron393@gmail.com>
2016-10-03 22:57:10 +0900

    msm8974-common: UBERTC-4.9 kernel building preparation

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,6 +50,8 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9-cortex-a15/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 TARGET_KERNEL_SOURCE := kernel/htc/msm8974
 
 # QCOM hardware
